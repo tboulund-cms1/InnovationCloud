c315c793179b531c225f08d5d3ed327623ef1c86
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-20 11:50:51 +0100

    Made footer.php dynamic

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
-<div class="footer">
+    <div class="footer">
       <div class="container">
         <p>© Copyright 2021</p>
       </div>
     </div>
+    <?php wp_footer() ?>
   </body>
 </html>
