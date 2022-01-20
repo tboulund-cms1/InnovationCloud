e9378217829d58b5d2ae6728fd66574921085f9f
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-20 09:20:20 +0100

    Made header.php dynamic

--- header.php
+++ header.php
@@ -2,11 +2,13 @@
   <head>
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,100' rel='stylesheet' type='text/css'>
     <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() ?>/style.css' />
+    <title><?php bloginfo("name") ?></title>
+    <?php wp_head() ?>
   </head>
   <body>
     <div class="header">
       <div class="container">
-        <h1>Innovation Cloud</h1>
-        <p>Connect your ideas globally</p>
+        <h1><?php bloginfo("name") ?></h1>
+        <p><?php bloginfo("description") ?></p>
       </div>
     </div>
