2999e57d0d0f2c258af501f030739bb1ffab618f
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-20 07:36:20 +0100

    Step 1 completed

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,100' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet' href='style.css' />
+  </head>
+  <body>
+    <div class="header">
+      <div class="container">
+        <h1>Innovation Cloud</h1>
+        <p>Connect your ideas globally</p>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container">
+        <img src="images/cloud.svg" height="128" width="196">
+        <h2>The Innovation Cloud Conference</h2>
+        <p>Connect with the best minds across a wide range of industries to share ideas and brainstorm new solutions to challenging problems.</p>
+        <p>Hear industry leaders talk about what worked (and what didn't) so that you can save time on your most challenging projects.</p>
+        <p>Learn about the latest research and technologies that you can use immediately to invent the future.</p>
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="container">
+        <p>© Copyright 2021</p>
+      </div>
+    </div>
+  </body>
+</html>
