9b6b3976166a53cc170e829405828385484815af
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-20 08:13:30 +0100

    Introduced WP loop

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     </div>
 
     <div class="main">
-      <div class="container">
-        <img src="images/cloud.svg" height="128" width="196">
-        <h2>The Innovation Cloud Conference</h2>
-        <p>Connect with the best minds across a wide range of industries to share ideas and brainstorm new solutions to challenging problems.</p>
-        <p>Hear industry leaders talk about what worked (and what didn't) so that you can save time on your most challenging projects.</p>
-        <p>Learn about the latest research and technologies that you can use immediately to invent the future.</p>
-      </div>
+        <?php if(have_posts()): ?>
+            <?php while(have_posts()): the_post() ?>
+                <?php the_content() ?>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <p>Sorry, we did not find any content to show here.</p>
+        <?php endif; ?>
     </div>
 
     <div class="footer">
