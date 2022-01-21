2f3c42f75c05a487357736fce2523e25a00613e4
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-21 11:29:13 +0100

    Implemented a search form

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 function innocloud_register_stylesheet() {
     wp_enqueue_style("theme-style", get_stylesheet_directory_uri() . "/style.css");
     wp_enqueue_style("roboto-font", "https://fonts.googleapis.com/css?family=Roboto:400,300,500,100");
+    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+    wp_enqueue_style("bootstrap-icons", "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css");
 }
 add_action("wp_enqueue_scripts", "innocloud_register_stylesheet");
 
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
         <?php the_custom_logo() ?>
         <h1><?php bloginfo("name") ?></h1>
         <p><?php bloginfo("description") ?></p>
+        <?php get_search_form() ?>
       </div>
     </div>
     <?php wp_nav_menu(array("theme_location" => "header-menu-location")) ?>
