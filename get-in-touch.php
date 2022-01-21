67e79ccca5d20565111dc06dfb17e2a3f8c6b89f
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-21 10:10:10 +0100

    Implemented a custom template

---
+++ get-in-touch.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Get In Touch template
+*/
+?>
+
+<?php get_header() ?>
+
+    <div class="container">
+        <h1>Get in touch</h1>
+        <p>You can find us on the following Social Media:</p>
+        <?php if(have_posts()): ?>
+            <?php while(have_posts()): the_post() ?>
+                <?php the_content() ?>
+            <?php endwhile ?>
+        <?php else: ?>
+            <p>No content found</p>
+        <?php endif ?>
+    </div>
+
+<?php get_footer() ?>
