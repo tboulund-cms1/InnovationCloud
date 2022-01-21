60d7713b467638502b3695bbc3ae6f1ed726220a
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-01-21 13:35:49 +0100

    Improved searching

--- search.php
+++ search.php
@@ -1,6 +1,7 @@
+<?php global $wp_query; ?>
 <?php get_header() ?>
 
-<p>We found the following search results:</p>
+<p>We found the following <b><?php echo $wp_query->found_posts ?></b> search results when searching for <?php echo $_GET["s"] ?>:</p>
 <ul>
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post() ?>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" action="<?php echo esc_url(home_url("/")) ?>">
-    <input class="form-control" type="text" name="s">
+    <input class="form-control" type="text" name="s" value="<?php echo $_GET["s"] ?>">
     <button type="submit" class="btn btn-danger form-control"><i class="bi bi-search"></i> Search</button>
 </form>
